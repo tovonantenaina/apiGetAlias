83f4f39a97960de9e565c6ad592bfe054399b728
Tovo RAKOTOZANANY <t.rakotozanany@pamf.mg>
2023-09-21 17:32:57 +0300

    test

---
+++ WebAppGetAlias/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppGetAlias.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
